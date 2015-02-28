c98548ec10d1202727929fd63fab39ef19fbc59d
Dylan W. Schwilk <dylan@schwilk.org>
2015-02-28 15:34:42 -0600

    fail and log if handbrake not found (fixes #27)
    - Put settings at top of encode script
    - Check before deleting mpeg

--- mythroku/rokuencode.sh
+++ mythroku/rokuencode.sh
@@ -2,16 +2,25 @@
 
 renice +15 --pid $$
 
+# settings:
+LOGFILE="/var/log/mythtv/rokuencode.log"
+DATABASEUSER=mythtv
+DATABASEPASSWORD=mythtv
+HANDBRAKE_PRESET=Normal
+REMOVE_MPEG=false
+
+# find HandBrakeCLI
+handbrake=$(which HandBrakeCLI)
+if [ ! -f $handbrake ]; then
+  date=`date`
+  echo "$date: rokuencode.sh requires HandBrakeCLI" >> $LOGFILE
+  exit 1
+fi
+
 #convert mpeg file to mp4 using handbrakecli
 MYTHDIR=$1
 MPGFILE=$2
 
-# Should try and get these from settings.php, but for now...
-DATABASEUSER=mythtv
-DATABASEPASSWORD=mythtv
-
-LOGFILE="/var/log/mythtv/rokuencode.log"
- 
 newbname=`echo $MPGFILE | sed 's/\(.*\)\..*/\1/'`
 newname="$MYTHDIR/$newbname.mp4"
 
@@ -20,7 +29,7 @@ echo "$date: Roku Encode $MPGFILE to $newname" >> $LOGFILE
 
 date=`date`
 echo "$newbname:$date Encoding" >> $LOGFILE
-/usr/bin/HandBrakeCLI --preset='iPhone & iPod Touch' -i $MYTHDIR/$MPGFILE -o $newname 
+$handbrake --preset="$HANDBRAKE_PRESET" -i $MYTHDIR/$MPGFILE -o $newname
 
 # update the db to point to the mp4
 date=`date`
@@ -33,13 +42,15 @@ date=`date`
 echo "$newname:$date Seek Table/update" >> $LOGFILE
 mythcommflag --file $newname --rebuild
 
-# remove the orignal mpg - note: when the recording is deleted using the roku, this original file will be deleted
-# , but not if deleted from mythtv frontend or mythweb.  see http://www.mythtv.org/wiki/Find_orphans.py for a solution.
-#date=`date`
-#echo "$MYTHDIR/$MPGFILE:$date File/remove" >> $LOGFILE
-#rm $MYTHDIR/$MPGFILE
+# remove the original mpg if doing so will not delete the new file. Note: when
+# the recording is deleted using the roku, this original file will be deleted ,
+# but not if deleted from mythtv frontend or mythweb. see
+# http://www.mythtv.org/wiki/Find_orphans.py for a solution.
+if [ $newname != $MYTHDIR/$MPGFILE ] & [ $REMOVE_MPEG ]; then
+    date=`date` echo "$MYTHDIR/$MPGFILE:$date File/remove" >> $LOGFILE
+    rm $MYTHDIR/$MPGFILE
+fi
 
 date=`date`
 echo "$date: Roku Encode $newbname Complete" >> $LOGFILE
 echo "" >> $LOGFILE
-
